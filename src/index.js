67109620478e2334763b17de13c4f551ddd385f0
Yrwynmyr <54967175+Bellfractus@users.noreply.github.com>
2019-10-13 15:37:51 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import '.public/index.html':
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
