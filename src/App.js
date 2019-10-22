07bfe3c15163d84b12a4a2a233fd6177b6795bb4
Yrwynmyr <54967175+Bellfractus@users.noreply.github.com>
2019-10-22 14:35:05 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
-        <a
-          className="App-link"
-          href="/public/index.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Welcome to Cybrynth</h1>
+     
       </header>
     </div>
   );
